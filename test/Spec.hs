a94f7a86d66f2714501ce4865e53928470b76167
Rustem Yamikov <jarustem@gmail.com>
2019-12-20 16:16:12 +0300

    Add quickcheck test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,33 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Char
+import           GHC.Generics
+import           LogicTest
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.Client
+import           Servant.QuickCheck
+import           Servant.Server
+import           Server
+import           System.IO
+import           Test.Hspec
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary
+import           Test.Tasty
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  hspec test1
+  defaultMain logicTests
+
+test1 :: Spec
+test1 =
+  describe "serverApi" $
+  it "not500" $
+  withServantServer serverApi (return server) $ \burl -> serverSatisfies serverApi burl args (not500 <%> mempty)
